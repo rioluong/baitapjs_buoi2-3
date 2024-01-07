9e0bf4adb3f91ff3933ebd4c90c42f849c95787e
rioluong <sang.khanhhn@gmail.com>
2024-01-07 22:49:24 +0700

    update code

--- index.js
+++ index.js
@@ -28,9 +28,38 @@ btnClick2.onclick = () => {
     ketQua.innerHTML =  `<i class="fa-solid fa-circle-arrow-right text-light"></i> Kết quả giá trị trung bình của bạn là: ${(num1 + num2 + num3 + num4 + num5)/5} `;
 };
 
-//   bài tập tập 3 quy đổi giá usd sang tiền việt việt
+//   bài tập 3 quy đổi giá usd sang tiền việt việt
   btnClick3.onclick = () => {
     var money = $('#txt-money').value * 1;
     var ketQua = $('.ketQua3');
     ketQua.innerHTML = `<i class="fa-solid fa-circle-arrow-right text-light"></i> Số tiền được quy đổi là : ${new Intl.NumberFormat("vn-VN").format(money * 23500)} VNĐ`;
   };
+
+  // Bài tập 4 tính chu vi và diện tích hình chữ nhật
+  btnClick4.onclick  = () => {
+    var chieuDai = $('#txt-dai').value * 1;
+    var chieuRong = $('#txt-rong').value * 1;
+    var ketQua = $('.ketQua4');
+    ketQua.innerHTML = `<i class="fa-solid fa-circle-arrow-right text-light"></i> Diện tích: ${chieuDai * chieuRong}; Chu vi: ${(chieuDai + chieuRong)*2}`;
+  };
+  //Bài tập 5 tính tổng 2 ký số
+  btnClick5.onclick = () => {
+    var num = $('#txt-number').value * 1;
+    var hangTram = 0;
+    var hangChuc = 0;
+
+    var ketQua = $('.ketQua5');
+    ketQua.innerHTML=`<i class="fa-solid fa-circle-arrow-right text-light"></i> 
+    ${Math.floor((num % 100) / 10) + (num % 10)}
+    `;
+  };
+  var inpuForm = $$(".txt-form-input");
+  inputForm.forEach(function (input){
+    input.addEventListener("input",function (event){
+      var inputValue = event.target.value;
+
+      var numericValue = inputValue.replace(/[^0-9]/g, "");
+
+      event.target.value = numericValue;
+    });
+  });
